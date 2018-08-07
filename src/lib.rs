fb23a31daa720a9da64c960885f3a9a52bc0d8cd
Horace Williams <horace.d.williams@gmail.com>
2018-08-06 21:45:58 -0700

    Debug cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,15 @@
-#![feature(float_extras)]
 extern crate geo_types;
 #[macro_use]
 extern crate lazy_static;
 
 use std::collections::HashMap;
 pub use geo_types::Coordinate;
-// use core::num::dec2flt::rawfp::RawFloat;
 
-fn debug_float(f: f64) -> () {
-    // let (mantissa, exponent, sign) = f.integer_decode();
-    println!("{}", f);
-    let long: u64 = unsafe { std::mem::transmute(f) };
-    // println!("{:b}{:b}{:b}", sign, exponent, mantissa);
-    println!("{:#066b}", long);
-
-    // let longMultiplied = (f * 0x80000000l) & 0x7fffffffl;
-    // let longBits: u64 = unsafe { std::mem::transmute(longMultiplied) };
-    // println!("{:#066b}", longBits);
-}
+// fn debug_float(f: f64) -> () {
+//     println!("{}", f);
+//     let long: u64 = unsafe { std::mem::transmute(f) };
+//     println!("{:#066b}", long);
+// }
 
 fn widen(mut low_32: u64) -> u64 {
   low_32 |= low_32 << 16; low_32 &= 0x0000ffff0000ffff;
@@ -33,26 +25,14 @@ const FLOAT_SHIFT: f64 = 0x80000000u32 as f64;
 pub fn encode(point: Coordinate<f64>, bits: usize) -> u64 {
     let biased_lon = (point.x + 180.0) / 360.0;
     let biased_lat = (point.y + 90.0) / 180.0;
-    debug_float(biased_lon);
-    debug_float(biased_lat);
 
     let lat_bits: u64 = ((biased_lat * FLOAT_SHIFT) as u64) & 0x7fffffff;
     let lon_bits: u64 = ((biased_lon * FLOAT_SHIFT) as u64) & 0x7fffffff;
 
-    // println!("{}", mult);
-    println!("{}", lon_bits);
-    println!("{}", lat_bits);
-    println!("{:#066b}", lon_bits);
-    println!("{:#066b}", lat_bits);
-
     let lat_wide = widen(lat_bits);
     let lon_wide = widen(lon_bits);
-    println!("{:#066b}", lon_wide);
-    println!("{:#066b}", lat_wide);
 
     let encoded = lon_wide | (lat_wide >> 1);
-    println!("****************8");
-    println!("{:#066b}", encoded);
     encoded >> (61 - bits)
 }
 
@@ -83,21 +63,16 @@ use std::collections::VecDeque;
 
 pub fn to_base_32(mut gh: u64, bits: usize) -> String {
     let mut chars: VecDeque<char> = VecDeque::with_capacity(bits / 5);;
-    for i in (0..bits / 5).rev() {
+    for _ in (0..bits / 5).rev() {
         let lookup_index = (gh & 0x1f) as usize;
-        println!("{}", lookup_index);
         let letter = BASE32[lookup_index];
         chars.push_front(letter);
-        // chars.push_front(BASE32_INV[&((gh & 0x1f) as usize)]);
         gh >>= 5;
     }
     chars.into_iter().collect()
 }
 
 pub fn encode_base_32(point: Coordinate<f64>, bits: usize) -> String {
-    for (index, char_code) in BASE32_INV_FAST.iter().enumerate() {
-        println!("{} - {}", index, char_code);
-    }
     to_base_32(encode(point, bits), bits)
 }
 
